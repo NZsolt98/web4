a18b6ee0eb4e6cab9673bf1d51f9ec6bff660851
Zsolt Nagy <nagyzsolt873@gmail.com>
2019-10-01 08:44:10 +0200

    Created a calculator and a counter. The calculator can sum,mul,div and kivon. The counter can count. In the index js i tested the calculator methods.

---
+++ JSReview2/Counter.js
@@ -0,0 +1,10 @@
+var counter=0;
+var increase=() => counter++;
+var decrease=() => counter--;
+var counter=()=>{return cnt};
+
+module.exports={
+    inc :increase,
+    dec :decrease,
+    val : counter
+}
---
+++ JSReview2/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+
+const calculator=require('./Calculator');
+
+console.log(calculator.add(5,3));
+console.mul(calculator.add(5,3));
+console.div(calculator.add(5,3));
