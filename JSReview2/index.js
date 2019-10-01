a5de314d880ffbaa1ba92402a6fc231fd4f4f3b7
Zsolt Nagy <nagyzsolt873@gmail.com>
2019-10-01 09:20:33 +0200

    Implemented counter as a set functions in a class and then tested it in the index.js file.

--- JSReview2/index.js
+++ JSReview2/index.js
@@ -1,18 +1,26 @@
 console.log("Hello World!");
 
 const calculator=require('./Calculator');
-const counter=require('./Counter');
 
 
 console.log(calculator.add(5,3));
 console.log(calculator.div(5,3));
 console.log(calculator.mul(5,3));
-console.log(counter.val());
-counter.inc();
-counter.inc();
-console.log(counter.val());
 
-const counter2=require('./Counter');
-console.log(counter2.val());
-counter2.inc();
-console.log(counter2.val());
+
+const Counter = require('./Counter');
+var cnt0 = new Counter(0);
+
+console.log(cnt0.value());
+cnt0.increase();
+cnt0.increase();
+cnt0.increase();
+console.log(cnt0.value());
+
+var cnt1 = new Counter(0);
+
+console.log(cnt1.value());
+cnt1.increase();
+cnt1.increase();
+cnt1.increase();
+console.log(cnt1.value());
